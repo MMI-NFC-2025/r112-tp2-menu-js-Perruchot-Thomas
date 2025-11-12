85b003ae1a1cb9cffc0e7be22a5e549b8ae905fa
Perruchot-Thomas <thomas.perruchot@edu.univ-fcomte.fr>
2025-11-12 11:59:00 +0100

    ouverture/fermeture menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,20 @@
+const toggle =
+document.querySelector(".menu-btn")
+
 const nav =
 document.querySelector(".menu")
+
+toggle.addEventListener("click", (evt)=>{
+    const isOpen = toggle.ariaExpanded === "true";
+const isClosed = !isOpen;
+console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+    nav.ariaHidden = isOpen;
+toggle.ariaExpanded = isClosed; 
+});
+
+
+
+
+
+ 
 
